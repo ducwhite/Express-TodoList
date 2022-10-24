c347612172c228122bca3617e282816599f54a92
DucDang <123dangminhduc@gmail.com>
2022-10-25 03:36:39 +0700

    Oct 25

---
+++ README.md
@@ -0,0 +1,11 @@
+# Install 
+
+```
+npm install ejs mongoose express
+```
+
+# Start server
+
+```
+node app.js || nodemon app.js
+```
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000
+// connect to mongodb
+mongoose.connect("mongodb://localhost/todo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+// routes
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
+
+
+// server configuration
+app.listen(3000, () => console.log(`Example app listening on port ${port}`));
